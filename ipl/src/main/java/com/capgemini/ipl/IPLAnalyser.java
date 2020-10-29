4aa031c88ac7b911d91ca2f1d9e275f6ab007e0b
LakshmiGanesh <71899781+LakshmiGanesh99@users.noreply.github.com>
2020-10-29 18:51:46 +0530

    [LakshmiGanesh] add: sorting method to sort cricketers who hit top 6s and 4s

--- ipl/src/main/java/com/capgemini/ipl/IPLAnalyser.java
+++ ipl/src/main/java/com/capgemini/ipl/IPLAnalyser.java
@@ -11,70 +11,92 @@ import java.util.function.Function;
 import com.google.gson.Gson;
 
 public class IPLAnalyser {
-	
-	public int loadBatsmenData(String csvFilePath ) throws IPLAnalyserException 
-	{
-		if(!(csvFilePath.matches(".*\\.csv$")))
+
+	public int loadBatsmenData(String csvFilePath) throws IPLAnalyserException {
+		if (!(csvFilePath.matches(".*\\.csv$")))
 			throw new IPLAnalyserException("Incorrect Type", IPLAnalyserExceptionType.INCORRECT_TYPE);
 		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
-			
-			ICsvBuilder csvBuilder = CsvBuilderFactory.createBuilder();	
+
+			ICsvBuilder csvBuilder = CsvBuilderFactory.createBuilder();
 			List<CSVIPLRecords> batsmenList = csvBuilder.getListFromCsv(reader, CSVIPLRecords.class);
-			return  batsmenList.size();
-			
-		}
-		catch (IOException e) {
+			return batsmenList.size();
+
+		} catch (IOException e) {
 			throw new IPLAnalyserException("Incorrect CSV File", IPLAnalyserExceptionType.CENSUS_FILE_PROBLEM);
-		}
-		catch (RuntimeException e) 
-		{ 
+		} catch (RuntimeException e) {
 			throw new IPLAnalyserException("Wrong Delimiter or Header", IPLAnalyserExceptionType.SOME_OTHER_ERRORS);
 		}
 	}
-	
-	//UC1 sorting of cricketer with top betting averages
-	
+
+	// UC1 sorting of cricketer with top betting averages
+
 	public String getSortedBatsmenListOnBattingAverage(String csvFilePath) throws IPLAnalyserException {
 		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
 			ICsvBuilder csvBuilder = CsvBuilderFactory.createBuilder();
 			List<CSVIPLRecords> batsmenList = csvBuilder.getListFromCsv(reader, CSVIPLRecords.class);
-			Function<CSVIPLRecords, Double> batsmanEntity=record->record.average;
-			Comparator<CSVIPLRecords> censusComparator=Comparator.comparing(batsmanEntity);
+			Function<CSVIPLRecords, Double> batsmanEntity = record -> record.average;
+			Comparator<CSVIPLRecords> censusComparator = Comparator.comparing(batsmanEntity);
 			this.sortBatsmenList(batsmenList, censusComparator);
-			String sortedStateCensusToJson=new Gson().toJson(batsmenList);
+			String sortedStateCensusToJson = new Gson().toJson(batsmenList);
 			return sortedStateCensusToJson;
-		} 
-		catch (IOException e) {
+		} catch (IOException e) {
 			throw new IPLAnalyserException("Incorrect CSV File", IPLAnalyserExceptionType.CENSUS_FILE_PROBLEM);
 		}
 	}
-	 //UC2 sorting of cricketers to know the top striking rate of batsman
-	 
+	// UC2 sorting of cricketers to know the top striking rate of batsman
+
 	public String getSortedBatsmenListOnTopStrikingRates(String csvFilePath) throws IPLAnalyserException {
 		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
 			ICsvBuilder csvBuilder = CsvBuilderFactory.createBuilder();
 			List<CSVIPLRecords> batsmenList = csvBuilder.getListFromCsv(reader, CSVIPLRecords.class);
-			Function<CSVIPLRecords, Double> batsmanEntity=record->record.strikeRate;
-			Comparator<CSVIPLRecords> censusComparator=Comparator.comparing(batsmanEntity);
+			Function<CSVIPLRecords, Double> batsmanEntity = record -> record.strikeRate;
+			Comparator<CSVIPLRecords> censusComparator = Comparator.comparing(batsmanEntity);
 			this.sortBatsmenList(batsmenList, censusComparator);
-			String sortedStateCensusToJson=new Gson().toJson(batsmenList);
+			String sortedStateCensusToJson = new Gson().toJson(batsmenList);
 			return sortedStateCensusToJson;
-		} 
-		catch (IOException e) {
+		} catch (IOException e) {
 			throw new IPLAnalyserException("Incorrect CSV File", IPLAnalyserExceptionType.CENSUS_FILE_PROBLEM);
 		}
 	}
+
+	// UC3 sorting of cricketers who hit maximum 6s and 4s
+
+	public String getSortedBatsmenListOnMostSixes(String csvFilePath) throws IPLAnalyserException {
+		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
+			ICsvBuilder csvBuilder = CsvBuilderFactory.createBuilder();
+			List<CSVIPLRecords> batsmenList = csvBuilder.getListFromCsv(reader, CSVIPLRecords.class);
+			Function<CSVIPLRecords, Integer> batsmanEntity = record -> record.sixes;
+			Comparator<CSVIPLRecords> censusComparator = Comparator.comparing(batsmanEntity);
+			this.sortBatsmenList(batsmenList, censusComparator);
+			String sortedStateCensusToJson = new Gson().toJson(batsmenList);
+			return sortedStateCensusToJson;
+		} catch (IOException e) {
+			throw new IPLAnalyserException("Incorrect CSV File", IPLAnalyserExceptionType.CENSUS_FILE_PROBLEM);
+		}
+	}
+
+	public String getSortedBatsmenListOnMostFours(String csvFilePath) throws IPLAnalyserException {
+		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
+			ICsvBuilder csvBuilder = CsvBuilderFactory.createBuilder();
+			List<CSVIPLRecords> batsmenList = csvBuilder.getListFromCsv(reader, CSVIPLRecords.class);
+			Function<CSVIPLRecords, Integer> batsmanEntity = record -> record.fours;
+			Comparator<CSVIPLRecords> censusComparator = Comparator.comparing(batsmanEntity);
+			this.sortBatsmenList(batsmenList, censusComparator);
+			String sortedStateCensusToJson = new Gson().toJson(batsmenList);
+			return sortedStateCensusToJson;
+		} catch (IOException e) {
+			throw new IPLAnalyserException("Incorrect CSV File", IPLAnalyserExceptionType.CENSUS_FILE_PROBLEM);
+		}
+	}
+
 	public void sortBatsmenList(List<CSVIPLRecords> batsmenList, Comparator<CSVIPLRecords> censusComparator) {
-		for(int i=0;i<batsmenList.size()-1;i++) 
-		{
-			for(int j=0; j<batsmenList.size()-i-1;j++)
-			{
-				CSVIPLRecords sortKey1=batsmenList.get(j);
-				CSVIPLRecords sortKey2=batsmenList.get(j+1);
-				if(censusComparator.compare(sortKey1, sortKey2)<0)
-				{
+		for (int i = 0; i < batsmenList.size() - 1; i++) {
+			for (int j = 0; j < batsmenList.size() - i - 1; j++) {
+				CSVIPLRecords sortKey1 = batsmenList.get(j);
+				CSVIPLRecords sortKey2 = batsmenList.get(j + 1);
+				if (censusComparator.compare(sortKey1, sortKey2) < 0) {
 					batsmenList.set(j, sortKey2);
-					batsmenList.set(j+1, sortKey1);
+					batsmenList.set(j + 1, sortKey1);
 				}
 			}
 		}
--- ipl/src/test/java/com/capgemini/ipl/IPLAnalyserTest.java
+++ ipl/src/test/java/com/capgemini/ipl/IPLAnalyserTest.java
@@ -16,49 +16,68 @@ public class IPLAnalyserTest {
 		try {
 			int recordsCount = new IPLAnalyser().loadBatsmenData(RIGHT_CENSUS_CSV);
 			assertEquals(100, recordsCount);
-		}
-		catch (IPLAnalyserException e) {
+		} catch (IPLAnalyserException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Test
 	public void givenWrongCsvFile_ShouldThrowIPLAnalyserExceptionOfTypeCensusFileProblem() {
 		try {
 			new IPLAnalyser().loadBatsmenData(WRONG_CENSUS_CSV);
-		}
-		catch(IPLAnalyserException e) {
+		} catch (IPLAnalyserException e) {
 			assertEquals(IPLAnalyserExceptionType.CENSUS_FILE_PROBLEM, e.exceptionType);
 		}
 	}
-	
+
 	@Test
 	public void givenWrongTypeCsvFile_ShouldThrowIPLAnalyserExceptionOfTypeIncorrectType() {
 		try {
 			new IPLAnalyser().loadBatsmenData(WRONGTYPE_CENSUS_CSV);
-		}
-		catch(IPLAnalyserException e) {
+		} catch (IPLAnalyserException e) {
 			assertEquals(IPLAnalyserExceptionType.INCORRECT_TYPE, e.exceptionType);
 		}
 	}
-	
+
 	@Test
 	public void givenSortedOnBattingAverageBatsmenList_ShouldReturnBestAveragedBatsman() {
 		try {
 			String sortedBatsmenJson = new IPLAnalyser().getSortedBatsmenListOnBattingAverage(RIGHT_CENSUS_CSV);
-			CSVIPLRecords[] batsmenListCsv=new Gson().fromJson(sortedBatsmenJson, CSVIPLRecords[].class);
+			CSVIPLRecords[] batsmenListCsv = new Gson().fromJson(sortedBatsmenJson, CSVIPLRecords[].class);
 			assertEquals("MS Dhoni", batsmenListCsv[0].player);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Test
 	public void givenSortedOnStrikingRatesBatsmenList_ShouldReturnTopStrikeRateBatsman() {
 		try {
 			String sortedBatsmenJson = new IPLAnalyser().getSortedBatsmenListOnTopStrikingRates(RIGHT_CENSUS_CSV);
-			CSVIPLRecords[] batsmenListCsv=new Gson().fromJson(sortedBatsmenJson, CSVIPLRecords[].class);
+			CSVIPLRecords[] batsmenListCsv = new Gson().fromJson(sortedBatsmenJson, CSVIPLRecords[].class);
 			assertEquals("Ishant Sharma", batsmenListCsv[0].player);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void givenSortedOnMostSixesBatsmenList_ShouldReturnTopStrikeRateBatsman() {
+		try {
+			String sortedBatsmenJson = new IPLAnalyser().getSortedBatsmenListOnMostSixes(RIGHT_CENSUS_CSV);
+			CSVIPLRecords[] batsmenListCsv = new Gson().fromJson(sortedBatsmenJson, CSVIPLRecords[].class);
+			assertEquals("Andre Russell", batsmenListCsv[0].player);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void givenSortedOnMostFoursBatsmenList_ShouldReturnTopStrikeRateBatsman() {
+		try {
+			String sortedBatsmenJson = new IPLAnalyser().getSortedBatsmenListOnMostFours(RIGHT_CENSUS_CSV);
+			CSVIPLRecords[] batsmenListCsv = new Gson().fromJson(sortedBatsmenJson, CSVIPLRecords[].class);
+			assertEquals("Shikhar Dhawan", batsmenListCsv[0].player);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
